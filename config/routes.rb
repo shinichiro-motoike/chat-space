44445cfc33b7e66a3a029454534ea09f679cdf18
shinchika34 <58651375+shinchika34@users.noreply.github.com>
2019-12-19 22:03:19 +0900

    add-login-function

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :messages
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
   root "messages#index"
+  resources :messages
+  resources :users, only: [:edit, :update]
 end
