ad6d8c994e8a338ad90402735aa77a469928a892
shinchika34 <58651375+shinchika34@users.noreply.github.com>
2019-12-22 14:14:33 +0900

    add-create-group

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "messages#index"
+  root "groups#index"
   resources :messages
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:index,:edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update]
 end
