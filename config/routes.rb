6ae49f811d238f8984904370485653ea93bd772e
shinchika34 <58651375+shinchika34@users.noreply.github.com>
2019-12-25 23:08:45 +0900

    add-massage-sending

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   root "groups#index"
   resources :messages
   resources :users, only: [:index,:edit, :update]
-  resources :groups, only: [:new, :create, :edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
+  end
 end
